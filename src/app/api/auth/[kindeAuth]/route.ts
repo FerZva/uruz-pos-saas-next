155234916188b7016a9d35bfb2fb79bd3e121b53
FerZva <fernandzva2019@outlook.es>
2025-04-20 15:00:57 -0600

    adding validation for sign in on live enviroment

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,2 +1,17 @@
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
-export const GET = handleAuth();
+import { NextRequest } from "next/server";
+
+export function GET(
+  request: NextRequest,
+  { params }: { params: { kindeAuth: string } }
+) {
+  const endpoint = params.kindeAuth;
+
+  if (endpoint === "logout") {
+    const response = handleAuth(request, params);
+    response.headers.set("Access-Control-Allow-Headers", "rsc");
+    return response;
+  } else {
+    return handleAuth(request, params);
+  }
+}
